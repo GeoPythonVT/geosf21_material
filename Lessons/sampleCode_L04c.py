b5621c60f53f7edb4e41f90590773a81effe2277
GeoPythonVT <swerth@vt.edu>
2021-09-13 21:47:24 -0400

    update

---
+++ Lessons/sampleCode_L04c.py
@@ -0,0 +1,31 @@
+restaurantsInOurTown = [
+    'La Madeleine',
+    'Pont Blanc',
+    'Olive Garden',
+    'Pommes de Terre',
+    'Jean"s''LaBoca', 
+    'Pont Blanc', 
+    'La Madeleine', 
+    'Berliner Kueche', 
+    'La Madeleine',
+    'Olive Garden']
+
+setFrench = set(['La Madeleine','Pont Blanc','Jean"s'])
+setPark = set(['LaBoca', 'Pont Blanc', 'La Madeleine', 'Berliner Kueche', 'Olive Garden'])
+
+# Is the restaurant 'Madeleine' located near a park?
+
+
+# Are french restaurants a category of park restaurants?
+
+
+# Are park restaurants a category of french restaurants?
+
+
+# We are just hungry, any restaurant is fine!
+
+
+# Find a french restaurant near a park!
+
+
+# Find a restaurant that is near a park but not french style!
