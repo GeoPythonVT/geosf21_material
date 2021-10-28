71b4f37575e972af23e7ca0a3135e9900cebdbdb
GeoPythonVT <swerth@vt.edu>
2021-10-28 14:20:25 -0400

    update

---
+++ Lessons/.ipynb_checkpoints/HelloWorld-checkpoint.py
@@ -0,0 +1,11 @@
+# Geo Data Science with Python, Susanna Werth, Fall 2021
+
+# This is a script file (.py) with Python code.
+
+print('Hello, world!')
+
+a = 6
+b = 7
+c = a+b
+
+print("The sum of %d and %d is: %d" %(a,b,c))
